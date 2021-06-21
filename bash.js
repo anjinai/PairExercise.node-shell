946a0569d4cd7ddf96c772433d6cf504f4a3ea1f
AnjiNai <anjali.nainani@gmail.com>
2021-06-21 15:07:57 -0400

    initial commit: completed sections 1, 2, and 3

---
+++ bash.js
@@ -0,0 +1,12 @@
+process.stdout.write("prompt > ");
+
+process.stdin.on("data", (data) => {
+  const cmd = data.toString().trim();
+  const pwd = require("./pwd");
+  const ls = require("./ls");
+  pwd();
+  ls();
+
+  //   process.stdout.write("You typed: " + cmd);
+  process.stdout.write("\nprompt > ");
+});
